6427b8a048676e551f7ac5a3bff2ed79fc857a63
Terence Svubure <tsvubure@gmail.com>
2024-03-05 16:29:25 +0100

    snake second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        int boardWidth = 600;
+        int boardheight = boardWidth;
 
+        JFrame frame = new JFrame("SNAKE");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardheight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardheight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
     }
 }
